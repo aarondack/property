d2188d84fdc20ce92cd9dc26f66a75b11df66361
alexcatanzaro <alexcatanzaro@gmail.com>
2017-11-08 21:29:49 -0600

    initial api commits

--- api/index.js
+++ api/index.js
@@ -2,3 +2,10 @@ const got = require("got");
 const env = require('dotenv').config();
 
 let witurl = 'https://api.wit.ai/message?v=20171108&q='
+let witsponse;
+
+function chgMessage(chatText) {
+  // get text from console to send to api
+};
+
+got(witurl).then((response) => {witsponse = response.body});
