3d26f3b1a54b14fdfd7ebee509ba35d4acebefe4
Dack <adackbusiness@gmail.com>
2017-11-10 22:22:10 -0500

    Change intent to complaint as per API change

--- console/index.js
+++ console/index.js
@@ -4,14 +4,16 @@ const witAPI = require("../api");
 const bot = new ConsoleBot();
 
 bot.onEvent(async context => {
-  const { entities: { intent } = {} } = await witAPI.query(context.event.text);
+  const { entities: { complaint } = {} } = await witAPI.query(
+    context.event.text
+  );
 
-  if (intent !== undefined && intent[0].value === "complain") {
+  if (complaint !== undefined && complaint[0].value === "complain") {
     await context.sendText(
       "We understand you are upset! What's your flight confirmation number?"
     );
   } else {
-    await context.sendText("Keep it on topic!");
+    await context.sendText("Here's a list of things you can do. To start, you can say 'File a complaint'""");
   }
 });
 
