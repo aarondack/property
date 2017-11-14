b506f2459780929a0599b712fee9705615f43aae
alexcatanzaro <alexcatanzaro@gmail.com>
2017-11-13 23:32:29 -0600

    Added a little handling for flight_info, more to come

--- console/index.js
+++ console/index.js
@@ -3,8 +3,8 @@ const witAPI = require("../api");
 
 const bot = new ConsoleBot();
 
-bot.onEvent(async context => {
-  const { entities: { complaint } = {} } = await witAPI.query(
+bot.onEvent( async context => {
+  const { entities: { complaint, flight_information, flight_number } = {} } = await witAPI.query(
     context.event.text
   );
 
@@ -12,8 +12,11 @@ bot.onEvent(async context => {
     await context.sendText(
       "We understand you are upset! What's your flight confirmation number?"
     );
-  } else {
-    await context.sendText("Here's a list of things you can do. To start, you can say 'File a complaint'""");
+  } else if (flight_information !== undefined && flight_information[0].value === "flight") {
+    await context.sendText("Please wait while we fetch information for your flight...");
+  }
+  else{
+    await context.sendText("Here's a list of things you can do. To start, you can say 'File a complaint'");
   }
 });
 
